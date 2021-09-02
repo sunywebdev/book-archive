329599e2d0d90096503315d03d965bd339a0a8ff
Shoyeb Mohammed Suny <suny.w68@gmail.com>
2021-09-02 11:04:41 +0600

    changes

--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@ const searchBooks = () => {
     const url = `https://openlibrary.org/search.json?q=${searchInput}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => displayBooks(data.docs))
-
+        .then(data => displayBooks(data))
 
 }
 /* const displayBooks = books => {
@@ -29,38 +28,34 @@ const searchBooks = () => {
                 `;
 } */
 
-const displayBooks = allBooks => {
-    console.log(allBooks)
-
+const displayBooks = books => {
+    console.log(books)
     const bookShelf = document.getElementById("books")
-    /*  const totalBooks = document.getElementById("total-books")
-     totalBooks.innerHTML = `
+    const totalBooks = document.getElementById("total-books")
+    totalBooks.innerHTML = `
                  <button type="button" class="btn btn-success">
                      Total Book Found <span class="badge bg-danger">${books.numFound}</span>
                  </button>
-                 `; */
+                 `;
+    const allBooks = books.docs.slice(0, 12);
     allBooks.forEach(book => {
         const div = document.createElement('div');
+        div.classList.add('col-md-3');
         div.classList.add('col-6');
+        div.classList.add('p-1');
         div.innerHTML = `
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid rounded-start" alt="...">
+                    <div class="card border-0 h-100">
+                        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top " alt="${book.title}">
+                        <div class="card-body pb-0">
+                            <h6 class="card-title">${book.title}</h6>
+                            <p class="card-title">By ${book.author_name}</p>
+                            <p class="published">First Published ${book.first_publish_year}</p>
                         </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                    <h4 class = "card-title" > ${book.title}</h4>
-                                    <h5 class="card-title">${book.author_name}</h5>
-                                    <h6 class="card-title">First Published in ${book.first_publish_year}</h6>
-                                    <a href="https://openlibrary.org/${book.key}" class="btn btn-primary">Read The Book</a>
-                            </div>
+                        <div class="card-footer border-0 p-0">
+                             <a href="https://openlibrary.org/${book.key}" class="btn btn-primary">Read The Book</a>
                         </div>
                     </div>
-                </div>
-
-                `;
+                    `;
         bookShelf.appendChild(div);
     })
-
 }
