add821f45284cff65ffd8b4efa33b23f42646bce
Shoyeb Mohammed Suny <suny.w68@gmail.com>
2021-09-02 13:24:13 +0600

    main finished

--- js/script.js
+++ js/script.js
@@ -1,39 +1,46 @@
-    var bookShelf = document.getElementById("books")
-    const searchBooks = () => {
-        const search = document.getElementById("search")
-        const searchInput = search.value;
-
-        search.value = ""; /* clear search input field */
-        if (searchInput === "") {
-            bookShelf.innerHTML = `
+var bookShelf = document.getElementById("books")
+var totalBooks = document.getElementById("total-books")
+const searchBooks = () => {
+    const search = document.getElementById("search")
+    const searchInput = search.value;
+    search.value = ""; /* clear search input field */
+    if (searchInput === "") {
+        bookShelf.innerHTML = `
             <div class="alert alert-danger" role="alert">
                 Please Enter Book Name
             </div>
     `;
-        } else {
-            spinner(); /* showing spinner after clicking search button */
-            const url = `https://openlibrary.org/search.json?q=${searchInput}`;
-            fetch(url)
-                .then(res => res.json())
-                .then(data => displayBooks(data))
+    } else {
+        spinner(); /* showing spinner after clicking search button */
+        const url = `https://openlibrary.org/search.json?q=${searchInput}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayBooks(data))
 
-        }
     }
+}
 
-    /* spinner function */
-    const spinner = () => {
-        bookShelf.innerHTML = `
+/* spinner function */
+const spinner = () => {
+    bookShelf.innerHTML = `
             <button class="btn btn-primary" type="button" disabled>
             <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
             Loading... Please Wait For A Moment
             </button>
     `;
-    }
+    totalBooks.innerHTML = "";
+}
 
-    const displayBooks = books => {
-        console.log(books)
+const displayBooks = books => {
+    console.log(books)
+    if (books.numFound === 0) {
+        bookShelf.innerHTML = `
+            <div class="alert alert-danger" role="alert">
+                No Result Found, Please Try Again.
+            </div>
+            `;
+    } else {
         /* Show total books number found on search results */
-        const totalBooks = document.getElementById("total-books")
         totalBooks.innerHTML = `
                  <button type="button" class="btn btn-success">
                      Total Book Found <span class="badge bg-danger">${books.numFound}</span>
@@ -62,6 +69,7 @@
             bookShelf.appendChild(div);
         })
     }
+}
 
 
 
@@ -71,24 +79,24 @@
 
 
 
-    /* const displayBooks = books => {
-        console.log(books);
-        const bookShelf = document.getElementById("books")
-        const totalBooks = document.getElementById("total-books")
-        totalBooks.innerHTML = `
-                    <button type="button" class="btn btn-success">
-                        Total Book Found <span class="badge bg-danger">${books.numFound}</span>
-                    </button>
-                    `;
-        bookShelf.innerHTML = `
-                    <div class="card" style="width: 18rem;">
-                    <img src="https://covers.openlibrary.org/b/id/${books.docs[0].cover_i}-M.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                            <h4 class = "card-title" > ${books.docs[0].title}</h4>
-                            <h5 class="card-title">${books.docs[0].author_name[0]}</h5>
-                            <h6 class="card-title">First Published in ${books.docs[0].first_publish_year}</h6>
-                            <a href="https://openlibrary.org/${books.docs[0].key}" class="btn btn-primary">Read The Book</a>
-                        </div>
+/* const displayBooks = books => {
+    console.log(books);
+    const bookShelf = document.getElementById("books")
+    const totalBooks = document.getElementById("total-books")
+    totalBooks.innerHTML = `
+                <button type="button" class="btn btn-success">
+                    Total Book Found <span class="badge bg-danger">${books.numFound}</span>
+                </button>
+                `;
+    bookShelf.innerHTML = `
+                <div class="card" style="width: 18rem;">
+                <img src="https://covers.openlibrary.org/b/id/${books.docs[0].cover_i}-M.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                        <h4 class = "card-title" > ${books.docs[0].title}</h4>
+                        <h5 class="card-title">${books.docs[0].author_name[0]}</h5>
+                        <h6 class="card-title">First Published in ${books.docs[0].first_publish_year}</h6>
+                        <a href="https://openlibrary.org/${books.docs[0].key}" class="btn btn-primary">Read The Book</a>
                     </div>
-                    `;
-    } */
+                </div>
+                `;
+} */
