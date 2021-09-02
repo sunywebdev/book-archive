129b6b2e8cd1491f361fd10e9d06f347ec9ef3f8
Shoyeb Mohammed Suny <suny.w68@gmail.com>
2021-09-02 16:13:57 +0600

    d

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const searchBooks = () => {
     search.value = ""; /* clear search input field */
     if (searchInput === "") {
         bookShelf.innerHTML = `
-            <div class="alert alert-danger" role="alert">
+            <div class="alert alert-danger  text-center" role="alert">
                 Please Enter Book Name
             </div>
     `;
@@ -16,14 +16,13 @@ const searchBooks = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => displayBooks(data))
-
     }
 }
 
 /* spinner function */
 const spinner = () => {
     bookShelf.innerHTML = `
-            <button class="btn btn-primary" type="button" disabled>
+            <button class="btn btn-primary  text-center" type="button" disabled>
             <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
             Loading... Please Wait For A Moment
             </button>
@@ -35,14 +34,14 @@ const displayBooks = books => {
     console.log(books)
     if (books.numFound === 0) {
         bookShelf.innerHTML = `
-            <div class="alert alert-danger" role="alert">
+            <div class="alert alert-danger  text-center" role="alert">
                 No Result Found, Please Try Again.
             </div>
             `;
     } else {
         /* Show total books number found on search results */
         totalBooks.innerHTML = `
-                 <button type="button" class="btn btn-success">
+                 <button type="button" class="btn btn-success text-center">
                      Total Book Found <span class="badge bg-danger">${books.numFound}</span>
                  </button>
                  `;
@@ -52,19 +51,23 @@ const displayBooks = books => {
         bookShelf.innerHTML = ` `; /* clear books results after a new search */
         allBooks.forEach(book => {
             const div = document.createElement('div');
-            div.classList.add('col-md-3');
+            div.classList.add('col-md-6');
             div.innerHTML = `
-                    <div class ="card border-0 h-100" >
-                        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top " alt="${book.title}">
-                        <div class="card-body pb-0">
-                            <h6 class="card-title">${book.title}</h6>
-                            <p class="card-title">By ${book.author_name}</p>
-                            <p class="published">First Published ${book.first_publish_year}</p>
-                        </div>
-                        <div class="card-footer border-0 pt-0">
-                             <a href="https://openlibrary.org/${book.key}" class="btn btn-primary">Read The Book</a>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid rounded-start mx-auto d-block  align-self-center" alt="${book.title}">
+                    </div>
+                    <div class="col-md-8 align-self-center">
+                        <div class="card-body">
+                            <h4 class="card-title">${book.title}</h4>
+                            <h6 class="card-text">By ${book.author_name[0]}</h6>
+                            <p class="card-text">First published in ${book.first_publish_year}</p>
+                            <a href="https://openlibrary.org/${books.docs[0].key}" class="btn">Read The Book</a>
                         </div>
                     </div>
+                </div>
+            </div>
                     `;
             bookShelf.appendChild(div);
         })
@@ -80,6 +83,7 @@ const displayBooks = books => {
 
 
 /* const displayBooks = books => {
+     <h4>Publisher <span class=" text-primary">: ${book.publisher[0]}</span></h4>
     console.log(books);
     const bookShelf = document.getElementById("books")
     const totalBooks = document.getElementById("total-books")
